1b43996098d563d36854fac6f78d275c26cf8b51
Artur-1995 <89254423508@mail.ru>
2025-11-12 17:33:12 +0300

    Доработка умного поиска

--- public/js/app.js
+++ public/js/app.js
@@ -26697,6 +26697,67 @@ __webpack_require__.r(__webpack_exports__);
 
 window.Alpine = alpinejs__WEBPACK_IMPORTED_MODULE_1__["default"];
 alpinejs__WEBPACK_IMPORTED_MODULE_1__["default"].start();
+document.addEventListener("DOMContentLoaded", function () {
+  var searchEl = document.querySelector("#search");
+  console.log("Поиск элемента:", searchEl);
+  if (!searchEl) {
+    console.error("Элемент поиска не найден!");
+    return;
+  }
+  $(searchEl).select2({
+    placeholder: "Поиск статей...",
+    allowClear: true,
+    // Позволяет очищать поле
+    tags: true,
+    // Разрешить свободный ввод текста
+    ajax: {
+      url: function url(params) {
+        return "/blog/suggestions/" + encodeURIComponent(params.term); // Добавляем фразу в URL
+      },
+      dataType: 'json',
+      delay: 250,
+      // Задержка перед отправкой запроса (в мс)
+      processResults: function processResults(data) {
+        console.log("Предложения получены:", data);
+        return {
+          results: data.map(function (item) {
+            return {
+              id: item.value,
+              // Уникальный идентификатор
+              text: item.label,
+              // Видимый текст
+              url: item.url // Адрес статьи
+            };
+          })
+        };
+      },
+      cache: true // Кэшировать результаты для ускорения последующих запросов
+    },
+    escapeMarkup: function escapeMarkup(markup) {
+      return markup;
+    },
+    // Для поддержки HTML-тегов
+    templateResult: formatItem // Форматирование отображения элемента
+    // templateSelection: formatItem // Форматирование выбранного элемента
+  });
+
+  // Функция для форматирования элементов
+  function formatItem(item) {
+    if (item.loading) return item.text;
+
+    // Оборачиваем элемент в ссылку
+    return "\n                <a href=\"/blog/".concat(item.id, "-").concat(item.text, "\" target=\"_blank\" class=\"select2-result-link\">\n                    ").concat(item.text, "\n                </a>\n            ");
+  }
+  // Обработчик клика на элемент выпадающего списка
+  $(searchEl).on('select2:select', function (e) {
+    var selectedItem = e.params.data;
+    window.location.href = '/blog/' + selectedItem.id + '-' + selectedItem.text; // Перенаправление на страницу статьи
+  });
+  // Мониторим изменения в поле поиска
+  searchEl.addEventListener("change", function () {
+    console.log("Новый текст в поле поиска:", this.value);
+  });
+});
 
 /***/ }),
 
